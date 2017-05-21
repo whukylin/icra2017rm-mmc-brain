88c017d1ba65bfe9786a8ea63a9a340b022800e3
jwwangchn <jwwangchn@outlook.com>
2017-05-21 15:29:59 +0800

    sync

---
+++ kylindef.h
@@ -0,0 +1,40 @@
+#ifndef __KYLINDEF_H__
+#define __KYLINDEF_H__
+
+#define FS_KEY_H        (((uint32_t)1u)<<0)
+#define FS_KEY_L        (((uint32_t)1u)<<1)
+#define FS_KEY_M        (((uint32_t)1u)<<2)
+#define FS_KEY_N        (((uint32_t)1u)<<3)
+#define FS_LED_RED      (((uint32_t)1u)<<4)
+#define FS_LED_GREEN    (((uint32_t)1u)<<5)
+#define FS_SONAR_F      (((uint32_t)1u)<<6)
+#define FS_SONAR_M      (((uint32_t)1u)<<7)
+#define FS_SONAR_L      (((uint32_t)1u)<<8)
+#define FS_SONAR_R      (((uint32_t)1u)<<9)
+
+#define WDG_NUM              22u
+
+#define WDG_IDX_RCV          0u
+#define WDG_IDX_TTY          1u
+#define WDG_IDX_BTM          2u
+#define WDG_IDX_DBI          3u
+#define WDG_IDX_IMU          4u
+#define WDG_IDX_VRC          5u
+#define WDG_IDX_VHC          6u
+#define WDG_IDX_VDBUS        7u
+#define WDG_IDX_CBUS         8u
+#define WDG_IDX_CALIB        9u
+#define WDG_IDX_ZGYRO        10u
+#define WDG_IDX_MOTOR1       11u
+#define WDG_IDX_MOTOR2       12u
+#define WDG_IDX_MOTOR3       13u
+#define WDG_IDX_MOTOR4       14u
+#define WDG_IDX_MOTOR5       15u
+#define WDG_IDX_MOTOR6       16u
+#define WDG_IDX_KYLIN        17u
+#define WDG_IDX_SR04F        18u
+#define WDG_IDX_SR04M        19u
+#define WDG_IDX_SR04L        20u
+#define WDG_IDX_SR04R        21u
+
+#endif
