08f4d74126e04f3766d3da022299afb3c1c00b80
levenberg <353817175@qq.com>
2017-05-24 18:59:31 +0800

    堆叠8个盒子后，继续搬盒子

--- main.cpp
+++ main.cpp
@@ -45,6 +45,9 @@
 #define AXISX 0
 #define AXISY 2000
 
+//基地区新加盒子的坐标 addaxisX addaxisY
+#define ADDAXISX -100
+#define ADDAXISY 3000
 //TODO: 放置盒子的时候, 每一堆非第一个盒子放置的位置
 #define FIXED_ULTRASONIC_2_PUTBOX 100
 // 放置盒子的时候, 每一堆第一个盒子放置的位置
@@ -243,7 +246,7 @@ string workStateCout;
 string workStageCout;
 
 int putBoxNum = 1;
-int boxNum = 1; //the num of the box
+int boxNum = 1, addboxNum=0; //the num of the box
 int GraspTp;
 int GraspBw;
 int GraspOp;
@@ -1499,6 +1502,20 @@ int main(int argc, char **argv)
                         putBoxState = 1;
                     }
                 }
+                else if(addboxNum>0)
+                {
+                    detection_mode = 0;                           //关闭视觉
+                    txKylinMsg.cbus.fs |= 1u << CONTROL_MODE_BIT; //切换到绝对位置控制模式
+
+                    //到达目的地(基地区位置)
+                    //基地区坐标为(AXISX, AXISY)
+                    txKylinMsg_xyz_Fun(ADDAXISX + kylinOdomCalib.cbus.cp.x, X_SPEED_3 * ramp, ADDAXISY- (addboxNum-1)*40 + kylinOdomCalib.cbus.cp.y, Y_SPEED_3_FIRSTBOX * ramp, kylinOdomCalib.cbus.cp.z, Z_SPEED_3 * ramp);
+                    txKylinMsg_ec_Fun(0, 0, 0, 0);
+                    if (absoluteDistance < 10)
+                    {
+                        putBoxState = 1;
+                    }
+                }
                 else
                 {
                     //跳变部分在函数体里面
@@ -1667,10 +1684,11 @@ int main(int argc, char **argv)
         //根据最大搬运盒子数量, 修改这里的值
         if (boxNum == MAX_BOXNUM + 1)
         {
-            //missionEndTimeUs = currentTimeUs();
-            //cout << "mission time cost:" << (missionEndTimeUs - missionStartTimeUs) * 1e-6 << endl;
-            break;
+            addboxNum++;
+            if(addboxNum>MAX_BOXNUM+1)
+                break;
         }
+        
     }
     //if(capture.isOpened())
     //	capture.release();
